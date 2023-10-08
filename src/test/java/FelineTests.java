7085401dcbdbdaffecae45faf58379a4cf378244
dtelkov <d.telkov@cft.ru>
2023-10-08 17:12:09 +0700

    коммит проекта

---
+++ src/test/java/FelineTests.java
@@ -0,0 +1,41 @@
+import com.example.Cat;
+import com.example.Feline;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class FelineTests {
+    @Test
+    public void checkEatMeat() throws Exception {
+        Feline feline = new Feline();
+        List<String> expectedFood = List.of("Животные", "Птицы", "Рыба");
+
+        assertEquals(feline.eatMeat(),expectedFood);
+    }
+
+    @Test
+    public void checkGetFamily() {
+        Feline feline = new Feline();
+        String expectedFamily = "Кошачьи";
+
+        assertEquals(feline.getFamily(),expectedFamily);
+    }
+
+    @Test
+    public void checkGetKittens() {
+        Feline feline = new Feline();
+        int expectedKittensCount = 1;
+
+        assertEquals(feline.getKittens(),expectedKittensCount);
+    }
+
+    @Test
+    public void checkGetKittensNewValue() {
+        Feline feline = new Feline();
+        int expectedKittensCount = 3;
+
+        assertEquals(feline.getKittens(3),expectedKittensCount);
+    }
+}
